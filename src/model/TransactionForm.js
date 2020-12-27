564bcfaa409902a125538c642e40b1fcd5169ff8
jordivilagut <vilagutjordi@gmail.com>
2020-12-27 13:09:20 +0100

    Minor

--- src/model/TransactionForm.js
+++ src/model/TransactionForm.js
@@ -3,6 +3,6 @@ export class TransactionForm {
     constructor(amount, description, expenseType) {
         this.amount = amount;
         this.description = description;
-        this.expenseType = expenseType
+        this.expenseType = expenseType;
     }
 }
