22f14989f3720ffc83898a1df1430c9ea4d1bf36
jordivilagut <vilagutjordi@gmail.com>
2020-12-27 08:28:22 +0100

    Create SearchAndAction component

---
+++ src/components/search-and-action/SearchAndAction.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {SearchBox} from "../searchbox/SearchBox";
+import {Button} from "../button/Button";
+import "./SearchAndAction.scss"
+
+export const SearchAndAction = ({searchPlaceholder, searchBoxHandler, buttonAction, buttonText}) => (
+    <div className="searchAction">
+        <SearchBox
+            placeholder={searchPlaceholder}
+            onChangeHandler={searchBoxHandler}/>
+        <Button
+            action={buttonAction}
+            mainText={buttonText}/>
+    </div>
+)
