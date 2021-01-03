ffedccc338c2e1d7e8be04a7e741a9ae4428b7f9
jordivilagut <vilagutjordi@gmail.com>
2021-01-03 13:10:25 +0100

    Deactivate buttons

--- src/pages/static-homepage/StaticHomepage.js
+++ src/pages/static-homepage/StaticHomepage.js
@@ -14,30 +14,30 @@ export const StaticHomepage = () => {
         history.push("login")
     }
 
-    return <div className="userPlans">
-        <UserPlan
-            action={getStartedHandler}
-            imageUrl="https://cdn02.visitbarcelona.com/files/5445-8234-Imagen/Nit_Barcelona_h1.jpg"
-            title="Investor"
-            titleClassname="zelda"
-            buttonText={t("get.started")}
-            buttonColor="0292B7"
-            buttonTextColor="FFFFFF"/>
-        <UserPlan
-            action={getStartedHandler}
-            imageUrl="https://jw-webmagazine.com/wp-content/uploads/2019/06/jw-5d1448522b66d3.01045017.jpeg"
-            title="Start-Up"
-            titleClassname="dakwart"
-            buttonText={t("coming.soon")}
-            buttonColor="dddddd"
-            buttonTextColor="FFFFFF"/>
-        <UserPlan
-            action={getStartedHandler}
-            imageUrl="https://static.billboard.com/files/media/nyc-skyline-2016-billboard-1548-compressed.jpg"
-            title="Enterprise"
-            titleClassname="cyberpunk"
-            buttonText={t("coming.soon")}
-            buttonColor="dddddd"
-            buttonTextColor="FFFFFF"/>
+    return <div>
+        <div className="userPlans">
+            <UserPlan
+                action={getStartedHandler}
+                imageUrl="https://cdn02.visitbarcelona.com/files/5445-8234-Imagen/Nit_Barcelona_h1.jpg"
+                title="Investor"
+                titleClassname="zelda"
+                buttonText={t("get.started")}
+                buttonColor="0292B7"
+                buttonTextColor="FFFFFF"/>
+            <UserPlan
+                imageUrl="https://jw-webmagazine.com/wp-content/uploads/2019/06/jw-5d1448522b66d3.01045017.jpeg"
+                title="Start-Up"
+                titleClassname="dakwart"
+                buttonText={t("coming.soon")}
+                buttonColor="dddddd"
+                buttonTextColor="FFFFFF"/>
+            <UserPlan
+                imageUrl="https://static.billboard.com/files/media/nyc-skyline-2016-billboard-1548-compressed.jpg"
+                title="Enterprise"
+                titleClassname="cyberpunk"
+                buttonText={t("coming.soon")}
+                buttonColor="dddddd"
+                buttonTextColor="FFFFFF"/>
+        </div>
     </div>
 }
