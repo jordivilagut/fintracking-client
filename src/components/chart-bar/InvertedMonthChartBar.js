1ef2c8efa5ae5ce4e1477b8e5da9c7b8cbd90366
jordivilagut <vilagutjordi@gmail.com>
2021-03-06 10:16:52 +0100

    Fix chart bar with 0 height bug

--- src/components/chart-bar/InvertedMonthChartBar.js
+++ src/components/chart-bar/InvertedMonthChartBar.js
@@ -2,9 +2,11 @@ import React from "react";
 
 export const InvertedMonthChartBar = ({month, max, extraClass}) => {
     const value = month.budgetExpenses/max * 140
+    const height = Number.isNaN(value) ? 0 : value;
+
     return <div className="barFrame inverted">
         <div className={extraClass}
-             style={{height: `${value}px`}}/>
+             style={{height: `${height}px`}}/>
         <p>{month.budgetExpenses}</p>
     </div>
 }
--- src/components/chart-bar/MonthChartBar.js
+++ src/components/chart-bar/MonthChartBar.js
@@ -2,10 +2,12 @@ import React from "react";
 
 export const MonthChartBar = ({month, max, extraClass}) => {
     const value = month.budgetIncome/max * 140
+    const height = Number.isNaN(value) ? 0 : value;
+
     return <div className="barFrame">
         <h3>{month.name}</h3>
         <div className={extraClass}
-             style={{height: `${value}px`}}/>
+             style={{height: `${height}px`}}/>
         <p>{month.budgetIncome}</p>
     </div>
 }
