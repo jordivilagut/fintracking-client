32004ebd772b105598185ada052177d47b6c3c39
jordivilagut <vilagutjordi@gmail.com>
2021-03-06 12:23:57 +0100

    Fix bug funds

--- src/components/financial-snapshot/FinancialSnapshot.js
+++ src/components/financial-snapshot/FinancialSnapshot.js
@@ -3,10 +3,11 @@ import {MonthlyBalance} from "../monthly-balance/MonthlyBalance";
 import "./FinancialSnapshot.scss";
 import {CurrentFunds} from "../current-funds/CurrentFunds";
 
-export const FinancialSnapshot = ({month, setMonth, funds, submitFunds, showModal, toggleModal, monthlySummary, refreshSummary}) => (
+export const FinancialSnapshot = ({month, setMonth, funds, setFunds, submitFunds, showModal, toggleModal, monthlySummary, refreshSummary}) => (
     <section id="financialSnapshot">
         <CurrentFunds
             funds={funds}
+            setFunds={setFunds}
             showModal={showModal}
             toggleModal={toggleModal}
             submitFunds={submitFunds}
