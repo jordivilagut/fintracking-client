c92f3d88e6b22e1a5d3d6073b957df8fe6632b79
jordivilagut <vilagutjordi@gmail.com>
2021-03-07 09:44:58 +0100

    Add option to mobile menu

--- src/pages/mobile-menu/MobileMenu.js
+++ src/pages/mobile-menu/MobileMenu.js
@@ -2,13 +2,14 @@ import {useTranslation} from "react-i18next";
 import React from "react";
 import {MobileMenuLink} from "./MobileMenuLink";
 import "./MobileMenu.scss"
-import {faHome, faMoneyBill, faUser} from "@fortawesome/free-solid-svg-icons";
+import {faChartLine, faHome, faMoneyBill, faUser} from "@fortawesome/free-solid-svg-icons";
 
 export const MobileMenu = ({user}) => {
     const {t} = useTranslation();
     return <div className="mobileMenu">
         <MobileMenuLink uri="/home" text={t("menu.home")} icon={faHome}/>
-        <MobileMenuLink uri="/budget" text={t("menu.budget")} icon={faMoneyBill}/>
+        <MobileMenuLink uri="/finance" text={t("menu.finance")} icon={faMoneyBill}/>
+        <MobileMenuLink uri="/budget" text={t("menu.budget")} icon={faChartLine}/>
         <MobileMenuLink uri={user != null ? "/profile" : "/login"} text={t("menu.profile")} icon={faUser}/>
     </div>
 }
