f56fee69b70840b6679dbeeef6237a74a79f75fc
jordivilagut <vilagutjordi@gmail.com>
2021-03-12 21:33:17 +0100

    Fix styles

--- src/components/chart-bar/InvertedMonthChartBar.js
+++ src/components/chart-bar/InvertedMonthChartBar.js
@@ -6,10 +6,16 @@ export const InvertedMonthChartBar = ({month, max}) => {
     const secondBar = 5 + Math.abs(month.budgetExpenses - month.expenses)/max * 140
     const exceedsBudget = month.expenses > month.budgetExpenses
 
+    const firstBarHeight = Number.isNaN(firstBar) ? 5 : firstBar;
+    const secondBarHeight = Number.isNaN(secondBar) ? 5 : secondBar;
+
+    console.log(firstBar)
+    console.log(secondBar)
+
     return <div className="barFrame inverted">
-        <div className="month-bar second red" style={{height: `${firstBar}px`}}/>
+        <div className="month-bar second red" style={{height: `${firstBarHeight}px`}}/>
         <div className="month-bar middle transparent" style={{height: `2px`}}/>
-        <div className={exceedsBudget ? "month-bar first red" : "month-bar first yellow"} style={{height: `${secondBar}px`}}/>
+        <div className={exceedsBudget ? "month-bar first red" : "month-bar first yellow translucid"} style={{height: `${secondBarHeight}px`}}/>
         <p>{month.expenses} / {month.budgetExpenses}</p>
     </div>
 }
--- src/components/chart-bar/MonthChartBar.js
+++ src/components/chart-bar/MonthChartBar.js
@@ -6,11 +6,14 @@ export const MonthChartBar = ({month, max}) => {
     const secondBar = 5 + Math.abs(month.budgetIncome - month.income)/max * 140
     const exceedsBudget = month.income > month.budgetIncome
 
+    const firstBarHeight = Number.isNaN(firstBar) ? 5 : firstBar;
+    const secondBarHeight = Number.isNaN(secondBar) ? 5 : secondBar;
+
     return <div className="barFrame">
         <h3>{month.name}</h3>
-        <div className="month-bar first green" style={{height: `${firstBar}px`}}/>
+        <div className="month-bar first green" style={{height: `${firstBarHeight}px`}}/>
         <div className="month-bar middle transparent" style={{height: `2px`}}/>
-        <div className={exceedsBudget ? "month-bar second green" : "month-bar second yellow"} style={{height: `${secondBar}px`}}/>
+        <div className={exceedsBudget ? "month-bar second green" : "month-bar second yellow translucid"} style={{height: `${secondBarHeight}px`}}/>
         <p>{month.income} / {month.budgetIncome}</p>
     </div>
 }
